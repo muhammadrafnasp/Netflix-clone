784ef3860043a1e953c4bc8a41ac51c8c8cb8cb4
muhammadrafnasp <rafnasbasheer@gmail.com>
2022-11-29 00:25:13 +0530

    message

---
+++ script.js
@@ -0,0 +1,81 @@
+// Question 1
+const targetDiv = document.getElementById('answer1');
+const btn = document.getElementById('question1');
+btn.onclick = function (){
+    if(targetDiv.style.display !=="none"){
+        targetDiv.style.display = "none"
+    }
+    else{
+        targetDiv.style.display = "block";
+    }
+}
+
+// Question 2
+
+const questoin2 = document.getElementById('answer2');
+const questoin2btn = document.getElementById('question2');
+
+questoin2btn.onclick = function (){
+    if(questoin2.style.display !== "none"){
+        questoin2.style.display = "none"
+    }
+    else{
+        questoin2.style.display ="block";
+    }
+}
+
+// Question 3
+
+const questoin3 = document.getElementById('answer3');
+const questoin3btn = document.getElementById('question3');
+
+questoin3btn.onclick = function (){
+    if(questoin3.style.display !== "none"){
+        questoin3.style.display = "none"
+    }
+    else{
+        questoin3.style.display ="block";
+    }
+}
+
+// Question 4
+
+const questoin4 = document.getElementById('answer4');
+const questoin4btn = document.getElementById('question4');
+
+questoin4btn.onclick = function (){
+    if(questoin4.style.display !== "none"){
+        questoin4.style.display = "none"
+    }
+    else{
+        questoin4.style.display ="block";
+    }
+}
+
+// Question 5
+
+const questoin5 = document.getElementById('answer5');
+const questoin5btn = document.getElementById('question5');
+
+questoin5btn.onclick = function (){
+    if(questoin5.style.display !== "none"){
+        questoin5.style.display = "none"
+    }
+    else{
+        questoin5.style.display ="block";
+    }
+}
+
+// Question 6
+
+const questoin6 = document.getElementById('answer6');
+const questoin6btn = document.getElementById('question6');
+
+questoin6btn.onclick = function (){
+    if(questoin6.style.display !== "none"){
+        questoin6.style.display = "none"
+    }
+    else{ 
+        questoin6.style.display ="block";
+    }
+}
